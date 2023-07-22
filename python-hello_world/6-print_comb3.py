95c89fbffcca6651aab9e4fc0f0746c56cbea972
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-22 22:51:11 +0000

    brains and materials

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
-for i in range(10):
+for i in range(1, 10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if i < 8 else "\n")
+        print(f"{i}{j}", end=", " if i != 9 or j != 8 else "\n")
