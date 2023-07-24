e932a9080784d5fb8bad09d815c09d68e2cbbc84
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 22:20:12 +0000

    primeandeven

---
+++ python-functions/5-prime.py
@@ -0,0 +1,10 @@
+def primeno(number):
+    if number < 2:
+        return False
+
+    # Check for factors from 2 to the square root of the number
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
