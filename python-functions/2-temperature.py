cf371191d32a453697bd7993e5c4e038b461692d
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 22:52:19 +0000

    temperature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,14 @@
-#fahrenheit to celsius
-def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * (5 / 9)
-    return celsius
+def approximate_float(number):
+    # Convert the number to a string
+    number_str = str(number)
+
+    # Remove trailing zeros after the decimal point
+    approximated_str = number_str.rstrip('0').rstrip('.')
+
+    # Convert back to float 
+    if '.' in approximated_str:
+        approximated_number = float(approximated_str)
+    else:
+        approximated_number = int(approximated_str)
+
+    return approximated_number
