5fee2a20d42598d2731bd317645678ffb1b28639
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 19:53:25 +0000

    inventing is a combination

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,5 @@
 for i in range(10):
-    for j in range(i + 1, 10):
+    for j in range(i + 1, 9):
         print("{}{}, ".format(i, j), end = '')
+    if i == 9:
+        print("{}{} ".format(j, i), end = '')
