e079389e729258a1d35eef7ec1b46d14ca016250
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 22:28:27 +0000

    temperature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,8 +1,4 @@
 #fahrenheit to celsius
-fahrenheit = 100
-
 def convert_to_celsius(fahrenheit):
-
-    return ((fahrenheit - 32) * (5 / 9))
-
-temperature_celsius = convert_to_celsius(fahrenheit)
+    celsius = (fahrenheit - 32) * (5 / 9)
+    return celsius
