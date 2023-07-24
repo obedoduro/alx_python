c9503d80ddc256dd324e8580cad06e5045f130ba
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 23:13:15 +0000

    temperature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,29 +1,8 @@
 #fahrenheit to celsius
-fahrenheit = -459.67
+#convert to censuis and strip
+def convert_to_celsius(fahrenheit):
 
+    return ((fahrenheit - 32) * (5 / 9))
 
-
-def approximate_float(fahrenheit):
-    # Convert the number to a string
-    number_str = str(fahrenheit)
-
-    # Remove trailing zeros after the decimal point
-    approximated_str = number_str.rstrip('0').rstrip('.')
-
-    # Convert back to float (if needed)
-    if '.' in approximated_str:
-        approximated_number = float(approximated_str)
-    else:
-        approximated_number = int(approximated_str)
-        print(approximated_number)
-    
-    return approximated_number
-
-def convert_to_celsius(temperature):
-    
-
- return ((temperature - 32) * (5 / 9))
-
-temperature =  approximate_float(fahrenheit) 
-temperature_celsius = convert_to_celsius(temperature)
+temperature_celsius = convert_to_celsius(fahrenheit)
 print(temperature_celsius)
