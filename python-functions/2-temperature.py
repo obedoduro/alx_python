6281188ab7c41fa655228bc2929235f62d509488
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 23:15:34 +0000

    password checker

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,8 +1,5 @@
 #fahrenheit to celsius
 #convert to censuis and strip
 def convert_to_celsius(fahrenheit):
-
-    return ((fahrenheit - 32) * (5 / 9))
-
-temperature_celsius = convert_to_celsius(fahrenheit)
-print(temperature_celsius)
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
