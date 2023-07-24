477c77b9369b5b442d638b130123c0a1cc119bd0
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-07-24 21:50:54 +0000

    temperature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,3 +1,17 @@
 #fahrenheit to celsius
+user_input = input("Please temperature in farenheit: ")
+
+# Convert the input to an integer (if needed)
+try:
+    fahrenheit = int(user_input)
+    print("You entered an integer:", fahrenheit)
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
+
+
 def convert_to_celsius(fahrenheit):
-    return fahrenheit - 32
+
+    return (fahrenheit - 32)
+
+temperature_celsius = convert_to_celsius(fahrenheit)
+print (temperature_celsius)
