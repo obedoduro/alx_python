fc2c3801845a89bc8a60ef02e89688c0b0879460
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-02 11:49:59 +0000

    classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+#intiazing the method square
+square = Square(5)
+print(square._Square__size)  
---
+++ python-classes/README.md
@@ -0,0 +1,3 @@
+with this project:
+python classes with objects
+1st project : an initizialing
