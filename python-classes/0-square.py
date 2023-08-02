24a1590faf2efa60ff86dbf3655dece296c1a160
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-02 14:26:59 +0000

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,6 +4,16 @@ class Square:
 
 #intiazing the method square
 my_square = Square(3)
-
 print(type(my_square))
 print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+    
