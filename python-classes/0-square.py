90e5415ccce588473aa53a394cf6bc484325f9e1
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-02 14:54:02 +0000

    classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,18 +2,15 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-#intiazing the method square
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
+# Example usage
+square = Square(3)
+print(type(square))
+print(square.__dict__)
 try:
-    print(my_square.size)
-except Exception as e:
+    print(square.size)
+except AttributeError as e:
     print(e)
-
 try:
-    print(my_square.__size)
-except Exception as e:
+    print(square.__size)
+except AttributeError as e:
     print(e)
-    
