ced2442b9e907c489e9468ad57aebcff6cb2a66d
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-02 20:12:44 +0000

    printing sequence

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+class Square:
+    def __init__(self, size=0):
+     self.__size = size
+
+    #to retrieve
+    def size(self):
+        return self.__size
+        
+    #size.setter
+    def size(self,value):
+        if not isinstance ( value, int):
+            raise TypeError("size must be an integer")
+
+        if value<0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+    
+    #area of square
+    def area(self):
+        return self.__size * self.__size
+
+    #myprint
+    def my_print(self):
+
+        for _ in range(self.__size):
+            print("#" * self.__size)
+
+        if self.__size == 0:
+            print()
+
+
+empty_square = Square()
+empty_square.my_print()
+
+valid_square = Square(3)
+valid_square.my_print()
