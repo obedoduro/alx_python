7de82058cc2dcb0a24ddbb30d01cf5ca6e3abb53
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-03 17:48:12 +0000

    update

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,14 +6,3 @@ class Square:
             raise ValueError("size must be >= 0")
         
         self.__size = size
-        
-        
-my_square_1 = Square(3)
-#print(type(my_square_1))
-#print(my_square_1.__dict__)
-
-print(Square(3))
-
-#print(my_square_1, end='')
-
-
