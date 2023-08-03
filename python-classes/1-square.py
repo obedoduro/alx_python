4da84341e62829fd614ae3a3461c63ec44ba35f9
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-03 23:50:46 +0000

    update1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,14 +5,16 @@ class Square:
     Attributes:
     size    The value passed to be squared.
     """
-    
+    __doc__ = message="documentation"
     def __init__(self, size=0):
+        """ initialisation of function to handle prerequire values"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         
         self.__size = size
+        
      
 
 my_square_ref = Square
