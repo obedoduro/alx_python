ccf9ac903306a8b5c60ffb49768711564679e177
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-06 20:00:25 +0000

    exact same object

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -12,8 +12,8 @@ class B(A):
 obj1 = A()
 obj2 = B()
 
-print(is_same_class(obj1, A)) 
-print(is_same_class(obj1, B))   
-print(is_same_class(obj2, B))  
-print(is_same_class(obj2, A))  
+is_same_class(obj1, A) 
+is_same_class(obj1, B)   
+is_same_class(obj2, B)  
+is_same_class(obj2, A)  
 
