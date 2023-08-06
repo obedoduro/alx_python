c7194811476d608d475d20bf53a4ae795144fde3
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-06 20:21:23 +0000

    exact same object

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,29 @@
+# finding out if same class or inherit 
+# class has an object we know of
+def is_kind_of_class(obj, a_class):
+    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
+
+# parent class
+class A:
+    pass
+
+#inherit class
+class B(A):
+    pass
+
+#a class
+class C():
+    pass
+
+#objects assigned
+obj1 = A()
+obj2 = B()
+obj3 = C()
+
+#output
+print(is_kind_of_class(obj1, A)) 
+print(is_kind_of_class(obj2, B)) 
+print(is_kind_of_class(obj2, A)) 
+print(is_kind_of_class(obj1, B)) 
+print(is_kind_of_class(obj3, A)) 
+print(is_kind_of_class(obj3, C))  
