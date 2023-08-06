08e878243b84cc8caacda387d98a9280024600a1
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-06 22:20:41 +0000

    rectangle

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,25 @@
+class BaseGeometry:
+    def area(self):
+        raise Exception ("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+bg = BaseGeometry() 
+#print(dir(bg))
+
+print(bg.integer_validator("my_int", 12))
+
+class Rectangle:
+    gert = BaseGeometry()
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height =  height 
+
+    gert.integer_validator('width',10)
+    gert.integer_validator('height',10)
