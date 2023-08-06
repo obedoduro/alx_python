e7a521f81129729193e6e496022feb22012f1d43
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-06 20:41:30 +0000

    only sub class of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,30 @@
+#only sub class of a class
+def inherits_from(obj, a_class):
+    return isinstance(obj, a_class) and type(obj) != a_class
+
+#parent class
+class A:
+    pass
+#inherited directly
+class B(A):
+    pass
+#inherited indirectly
+class C(B):
+    pass
+
+#a class
+class D():
+    pass
+
+#assign objects
+obj1 = A()
+obj2 = B()
+obj3 = C()
+obj4 = D()
+
+
+# print(inherits_from(obj1, A))
+# print(inherits_from(obj2, B))
+# print(inherits_from(obj3, A)) 
+# print(inherits_from(obj3, B)) 
+# print(inherits_from(obj4, D)) 
