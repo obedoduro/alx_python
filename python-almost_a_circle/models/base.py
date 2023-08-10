c21f6878b1047abbd3835ee63e2fdc4ecd5f49fa
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-10 14:44:02 +0000

    base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#first class created
+class Base:
+    __nb_objects = 0 
+
+    def __init__(self, id = None ):
+        #self._id = id
+        if(int, id!=None):
+            __nb_objects += __nb_objects
+            id = __nb_objects
+            print(id)
+        print(id)
+    print(id)
+    print(__nb_objects)
+    
