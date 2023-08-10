c3849ea68689d1a119b4cd4f25aefc4ba1d8d34c
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-10 21:38:47 +0000

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,58 +1,13 @@
-# # models/base.py
-
-# class Base:
-#     """
-#     This class is the base for all other classes in the project.
-#     It manages the 'id' attribute for instances.
-#     """
-
-#     __nb_objects = 0
-
-#     def __init__(self, id=None):
-#         """
-#         Initialize a new instance of the Base class.
-
-#         Args:
-#             id (int, optional): If provided, the 'id' attribute of the instance will be set to this value.
-#                                If not provided, a unique 'id' will be assigned based on the number of objects created.
-
-#         Returns:
-#             None
-#         """
-#         if id is not None:
-#             self.id = id
-#         else:
-#             Base.__nb_objects += 1
-#             self.id = Base.__nb_objects
-
-
-
-# models/base.py
-
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+"""python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
 class Base:
-    """
-    This class is the base for all other classes in the project.
-    It manages the 'id' attribute for instances.
-    """
-
+  
     __nb_objects = 0
-
+    
+    """"python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
+    """ python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
     def __init__(self, id=None):
-        """
-        Initialize a new instance of the Base class.
-
-        Args:
-            id (int, optional): If provided, the 'id' attribute of the instance will be set to this value.
-                               If not provided, a unique 'id' will be assigned based on the number of objects created.
-
-        Returns:
-            None
-        """
-
-        
-        """
-        if  id != None
-        """
+       
         if id is not None:
             self.id = id
 
