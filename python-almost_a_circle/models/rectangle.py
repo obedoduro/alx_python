28611293b9a880c67b03a406568dd4e536b9a15a
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-13 23:47:18 +0000

    import

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -60,7 +60,7 @@ class Rectangle(Base):
             raise TypeError ("height must be an integer")
 
         if value <= 0:
-            raise ValueError("Height must be > 0")
+            raise ValueError("height must be > 0")
         
         self.__height = value
 
