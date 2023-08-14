595814f1ddd9967089808a841f956bf477babf99
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-14 13:40:32 +0000

    display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -102,7 +102,8 @@ class Rectangle(Base):
     
     def display(self):
         """Display stdout the Rectangle instance with the character #"""
-        
+        self.__width =width
+        self.__height=height
 
         while height <= 5:
             while width <= 5:
