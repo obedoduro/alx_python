0132f09f4e83566ba50226102d3d6a40cb047383
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-14 19:42:01 +0000

    display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -117,24 +117,44 @@ class Rectangle(Base):
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
     
-    def update(self, *args):
+    def update(self, *args, **kwargs):
+        
+            
         """
         Assigning arguments to attributes
         
         Args:
             *args: Arguments in the order id, width, height, x and y 
+            **kwags: keyword argument represnting attribute key/vlaue pairs.
         """
+        if args:
 
-        if len(args) >= 1 :
-            self.id = args[0]
-        if len(args) >= 2 :
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+            if len(args) >= 1 :
+                self.id = args[0]
+            if len(args) >= 2 :
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["a"]
+            if "width" in kwargs:
+                self.id = kwargs["b"]
+            if "height" in kwargs:
+                self.id = kwargs["c"]
+            if "x" in kwargs:
+                self.id = kwargs["d"]
+            if "y" in kwargs:
+                self.id = kwargs["e"]
+
+    
+
+
 
            
         
