d5df51f234ad09c503dbdfc32bf687ec90dcc32c
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-17 12:22:39 +0000

    response header value

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env/python3
+
+import requests
+import sys
+
+"""
+print(__import__("my_module").__doc__)
+"""
+def  takes_a_url_and_display(url):
+
+
+    response = requests.get(url)
+    request_id = response.headers.get('X-Request-Id')
+    return request_id
+    
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
+
+if  __name__ == "__main__":
+    takes_a_url_and_display()
