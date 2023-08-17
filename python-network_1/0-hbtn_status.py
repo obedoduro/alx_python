f147738ca63b6b3c10fde109cd8f52886e97e740
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-17 11:45:17 +0000

    what is my status python

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,17 +4,18 @@ Fetches https://alu-intranet.hbtn.io/status with a get request
 """
 import requests
 
-# #def fetch_and_display_status():
-#     """
-#     Fetches from specified url and displsys it
-#     """
+def fetch_and_display_status():
+    """
+    Fetches from specified url and displsys it
+    """
 
 url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
 
 
-# if __name__ == "main":
-    # fetch_and_display_status()
+if __name__ == "main":
+    fetch_and_display_status()
+
 print("Body response:")
 print("\t- type:", type(response.text))
 print("\t- content:", response.text)
