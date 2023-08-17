05733489512d2bd7cc726c69770bb11a34c1eff6
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-17 16:19:17 +0000

    error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -35,7 +35,7 @@ def errorcode(url):
 
 if __name__ == "__main__":
     if len(sys.argv)  != 3:
-        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        print("Usage: {} <URL>".format(sys.argv[0]))
         sys.exit(1)
 
     url = sys.argv[1]
