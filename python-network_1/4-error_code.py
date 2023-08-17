4712a4160cf2b43ffac41e712315227ffa53e394
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-08-17 15:56:41 +0000

    error code

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -6,16 +6,20 @@ import sys
 """
 errorcode takes Url , send request to Url and display the body
 """
-def errorcode():
-    req = urllib.request.Request('http://www.python.org/fish.html')
-try:
-    urllib.request.urlopen(req)
-except urllib.error.HTTPError as e:
-    print(e.code)
-    print(e.read())  
+def errorcode(url):
+
+    try:
+        response = requests.get(url)
+        print("Response Body: ")
+        print(repsonse.text)
+
+        if reponse.status_code >= 400:
+            print(f"Error code: {response.status_code}")
+
+
+    except requests.exceptions.RequestException as e:
+        print("An error occurred:", e)  
 
 
 if __name__ == "__main__":
-    errorcode()
-
-
+    errorcode(url)
