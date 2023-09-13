92299e441630dd585633a52ef27048a237b4833d
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 19:01:06 +0000

    filter states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -17,9 +17,9 @@ def list_cities(username, password, database_name):
         cursor = connection.cursor()
 
         # Execute the SQL query to retrieve cities with their respective states
-        cursor.execute("SELECT cities.id," 
-        "cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id "
-        "ORDER BY cities.id ASC")
+        cursor.execute(f"SELECT cities.id," 
+        f"cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id "
+        f"ORDER BY cities.id ASC")
 
         # Fetch all the rows
         cities = cursor.fetchall()
