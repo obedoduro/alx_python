dcb3245e83239f747c3a1a5d3b76227105194b35
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 18:33:34 +0000

    sql injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -40,5 +40,5 @@ if __name__ == "__main__":
         password, database_name, state_name = sys.argv[2], sys.argv[3], sys.argv[4]
         search_states(username, password, database_name, state_name)
     else:
-        print("Usage: python script.py <username> "
+        print("Usage: python script.py <username> ",
         "<password> <database_name> <state_name>")
