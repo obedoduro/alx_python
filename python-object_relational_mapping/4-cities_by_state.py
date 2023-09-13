10ea8767839d0a023ef7440d400473a15bf1baad
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 18:47:47 +0000

    sql injection

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+import MySQLdb
+import sys
+
+
+def list_cities(username, password, database_name):
+    try:
+        # Connect to the MySQL server
+        connection = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            host='localhost',
+            port=3306,
+            db=database_name
+        )
+
+        # Create a cursor object to interact with the database
+        cursor = connection.cursor()
+
+        # Execute the SQL query to retrieve cities sorted by id
+        cursor.execute("SELECT * FROM cities ORDER BY id ASC")
+
+        # Fetch all the rows
+        cities = cursor.fetchall()
+
+        # Display the results
+        for city in cities:
+            print(city)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database_name>")
+    else:
+        username = sys.argv[1],
+        password, database_name = sys.argv[2], sys.argv[3]
+        list_cities(username, password, database_name)
