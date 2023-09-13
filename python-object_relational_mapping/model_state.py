cfbaddea03c48a4b47a3e522f9824e42de7de809
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 20:15:37 +0000

    filter states

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+# Example usage of creating tables:
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    # Replace 'your_username', 'your_password', and 'your_database' with your MySQL credentials and database name
+    engine = create_engine('mysql://your_username:your_password@localhost:3306/your_database')
+
+    # Create tables based on defined classes
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    session = Session(engine)
+
+    # You can use the 'State' class to interact with the 'states' table in your database
+    # Example: Create a new state
+    new_state = State(name="New State")
+    session.add(new_state)
+    session.commit()
+
+    # Close the session
+    session.close()
