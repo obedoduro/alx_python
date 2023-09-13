abbe6242a6f8d25cb1b349703acecf99d555c4d2
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 10:27:20 +0000

    filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,40 @@
+import MySQLdb
+import sys
+
+def list_states_starting_with_n(username, password, database_name):
+    try:
+        # Connnect to the MYSQL server
+        connection = MySQLdb.connect(
+            user = username,
+            passwd=password,
+            host = 'localhost',
+            port = 3306,
+            db=database_name
+            )
+
+        # Create cursor object to interact with database
+        cursor = connection.cursor()
+        
+        # Execute the SQL query to retrieve states starting with 'N' sorted by id
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+        # Fetch all the rows
+        states =  cursor.fetchall()
+
+        # Display the results
+        for state in states:
+            print(state)
+    
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database_name>")
+    else:
+        username, password, database_name = sys.argv[1], sys.argv[2], sys.arg[3]
+        list_states_starting_with_n ( username, password, database_name)
