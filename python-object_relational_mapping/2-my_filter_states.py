757cb2b34facc1d82b05c57e3cbf165695f91bbc
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 16:01:47 +0000

    filter states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -37,5 +37,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: python script.py <username> <password> <database_name> <state_name>")
     else:
-        username, password, database_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+        username = sys.argv[1]
+        password, database_name, state_name = sys.argv[2], sys.argv[3], sys.argv[4]
         search_states(username, password, database_name, state_name)
