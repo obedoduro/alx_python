0e3796128e5147ea079d676e650e37de061d8073
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 20:22:19 +0000

    first state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,12 +9,15 @@ class State(Base):
     Represents a state in a database.
 
     Attributes:
-        id (int): An auto-generated unique identifier for the state.
-        name (str): The name of the state with a maximum length of 128 characters.
+        id (int): An auto-generated unique 
+        identifier for the state.
+        name (str): The name of the state with 
+        a maximum length of 128 characters.
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, 
+    nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 # Example usage of creating tables:
@@ -22,8 +25,11 @@ if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
 
-    # Replace 'your_username', 'your_password', and 'your_database' with your MySQL credentials and database name
-    engine = create_engine('mysql://your_username:your_password@localhost:3306/your_database')
+    # Replace 'your_username', 'your_password', 
+    # and 'your_database' with your MySQL credentials 
+    # and database name
+    engine = create_engine(
+        'mysql://your_username:your_password@localhost:3306/your_database')
 
     # Create tables based on defined classes
     Base.metadata.create_all(engine)
@@ -31,7 +37,8 @@ if __name__ == "__main__":
     # Create a session to interact with the database
     session = Session(engine)
 
-    # You can use the 'State' class to interact with the 'states' table in your database
+    # You can use the 'State' class to 
+    # interact with the 'states' table in your database
     # Example: Create a new state
     new_state = State(name="New State")
     session.add(new_state)
