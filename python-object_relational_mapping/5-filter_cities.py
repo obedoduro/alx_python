a7adb1ffc06a6f0d30b8656ab4ae7eba43e63a9a
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-13 20:00:04 +0000

    all cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -40,8 +40,9 @@ def list_cities_by_state(username, password, database_name, state_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database_name> <state_name>")
+        print(f"Usage: python script.py <username>"
+              f" <password> <database_name> <state_name>")
     else:
-        username, password =  sys.argv[1], sys.argv[2]
+        username, password = sys.argv[1], sys.argv[2]
         database_name, state_name = sys.argv[3], sys.argv[4]
         list_cities_by_state(username, password, database_name, state_name)
