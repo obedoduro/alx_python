4e314aff14fc94cc9f4a69ad5139004a97493c68
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-14 14:51:07 +0000

    model sate

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -30,5 +30,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: python script.py <username> <password> <database_name>")
     else:
-        username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+        username, password = sys.argv[1], sys.argv[2]
+        database_name = sys.argv[3]
         list_states(username, password, database_name)
