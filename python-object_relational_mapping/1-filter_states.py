38ad7d0d84dce63bd16fdd963b350b46956ced69
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-14 14:17:44 +0000

    filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,7 @@ def list_states_starting_with_n(username, password, database_name):
         # Execute the SQL query to retrieve
         # states starting with 'N' sorted by id
         cursor.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' AND name = 'New York' ORDER BY id ASC")
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
         # Fetch all the rows
         states = cursor.fetchall()
