31722b879c10805a1016ff62c26d478c36888750
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-14 15:03:19 +0000

    all sql

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,14 +6,20 @@ from model_state import Base, State
 
 def list_states_with_letter_a(username, password, database_name):
     # Create a SQLAlchemy engine to connect to the MySQL server
-    engine = create_engine(f"mysql://{username}:{password}@localhost:3306/{database_name}")
+    engine = create_engine(f"mysql://{username}:"
+                           f"{password}"
+                           f"@localhost:3306/"
+                           f"{database_name}")
 
     # Create a session to interact with the database
     session = Session(engine)
 
     try:
-        # Query State objects containing the letter 'a' and sort them by states.id
-        states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+        # Query State objects containing the 
+        # letter 'a' and sort them by states.id
+        states_with_a = (session.query(State)
+                        .filter(State.name.like('%a%'))
+                        .order_by(State.id).all())
 
         # Display the results
         for state in states_with_a:
@@ -28,7 +34,9 @@ def list_states_with_letter_a(username, password, database_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database_name>")
+        print("Usage: python script.py <username>"
+              "<password> <database_name>")
     else:
-        username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+        username, password = sys.argv[1], sys.argv[2]
+        database_name = sys.argv[3]
         list_states_with_letter_a(username, password, database_name)
