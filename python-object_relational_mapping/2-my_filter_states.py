d28cedd9d7a9fc94d843b8cce77b1f61791010e7
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-14 14:21:21 +0000

    filter states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,8 @@ def search_states(username, password, database_name, state_name):
         cursor = connection.cursor()
 
         # Create the SQL query using format and user input
-        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        query = ("SELECT * FROM states "
+                "WHERE name = '{}' ORDER BY id ASC".format(state_name))
         cursor.execute(query)
 
         # Fetch all the rows
@@ -38,7 +39,8 @@ def search_states(username, password, database_name, state_name):
 if __name__ == "__main__":
     if len(sys.argv) == 5:
         username, password = sys.argv[1], sys.argv[2] 
-        database_name, state_name =  sys.argv[3], sys.argv[4]
+        database_name, state_name = sys.argv[3], sys.argv[4]
         search_states(username, password, database_name, state_name)
     else:
-        print("Usage: python script.py <username> <password> <database_name> <state_name>")
+        print("Usage: python script.py <username> "
+              "<password> <database_name> <state_name>")
