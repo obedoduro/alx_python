6356b045c008904eb632bdda3a19241567335726
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-14 15:33:06 +0000

    hello flask

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,7 +1,16 @@
+"""
+This is a sample module to run on
+host 0.0.0
+port 5000
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+"""
+This module helps route in the function hello_HBNB
+
+"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
