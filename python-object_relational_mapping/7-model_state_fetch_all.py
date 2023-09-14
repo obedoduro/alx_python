0c6c50d05b3d518b615057a85240ef9b248e4a75
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-14 14:45:01 +0000

    all sql

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -23,7 +23,7 @@ def list_states(username, password, database_name):
     except Exception as e:
         print(f"Error: {e}")
     finally:
-        # Close the session and engine
+        # Close the session and engine immediately
         session.close()
         engine.dispose()
 
