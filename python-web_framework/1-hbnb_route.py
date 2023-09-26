9e2a597b6dc8957dc2e76c1543e221aeebf727eb
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-09-26 22:06:01 +0000

    hbnb

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# Create a Flask web application
+app = Flask(__name__)
+
+# Define a route for the root path with strict_slashes=False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Define a route for /hbnb with strict_slashes=False
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+# Run the application on 0.0.0.0:5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
