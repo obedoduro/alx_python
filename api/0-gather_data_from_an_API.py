33e2dd4ed3ba0e43f2e6ab735390e15e9d450d3d
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-18 09:07:41 +0000

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,27 @@
 import requests
 import sys
 
-def get_employee_info(employee_id):
-    """
-    Retrieve and display employee's TODO list progress.
-
-    Args:
-        employee_id (int): The employee's ID.
-
-    Returns:
-        None
-    """
+def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
 
+    # Get employee details
     employee_response = requests.get(f"{base_url}/users/{employee_id}")
     employee_data = employee_response.json()
+    employee_name = employee_data.get("name")
 
+    # Get employee's TODO list
     todo_response = requests.get(f"{base_url}/users/{employee_id}/todos")
     todo_data = todo_response.json()
 
+    # Calculate the number of completed and total tasks
     total_tasks = len(todo_data)
     completed_tasks = sum(1 for task in todo_data if task.get("completed"))
 
-    print(f"Employee {employee_data['name']} is done with tasks ({completed_tasks}/{total_tasks}):")
-
+    # Print employee TODO list progress
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
     for task in todo_data:
         if task.get("completed"):
-            print(f"\t{task['title']}")
+            print(f"\t{task.get('title')}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -35,6 +30,6 @@ if __name__ == "__main__":
 
     try:
         employee_id = int(sys.argv[1])
-        get_employee_info(employee_id)
+        get_employee_todo_progress(employee_id)
     except ValueError:
-        print("Employee ID must be an integer")
+        print("Employee ID must be an integer.")
