13b92e3892a510620e194f7e65303a873553c1df
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-26 09:36:38 +0000

    readme

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-# Import the add function from add_0.py
-from add_0 import add
+#function to add a and b
+def add(a, b):
+    return a + b
+    
+#main function to start from
+def main():
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f'{a} + {b} = {result}')
 
-# Assign values to variables a and b
-a = 1
-b = 2
-
-# Calculate the result using the add function
-result = add(a, b)
-
-# Print the result with string formatting
-print(f'{a} + {b} = {result}')
+if __name__ == "__main__":
+    main()
