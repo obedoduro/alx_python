16bc8ac0ce464b0eb3d2378bf22488a36cfbf405
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-26 13:25:04 +0000

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -18,18 +18,8 @@ class Square:
             None
         """
         self.__size = size
-    return self.__size
+    
 
 # Example usage
 my_square = Square(3)
 # print()
-# #print(my_square.__dict__)
-# my_square_ref = Square
-# #print(my_square_ref(3))
-# # type(my_square_ref)
-def main():
-    my_square_ref = my_square
-    
-
-main()
-__init__
