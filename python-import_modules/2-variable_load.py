af49f1ad363a6359b8136158dfa9096588f07dd4
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-26 10:18:40 +0000

    import

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python3
-from variable_load_2 import a
-
-if __name__ == "__main__":
-    print(a)
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
