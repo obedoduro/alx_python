9f97f9cc8618d78b59ff42c0ed22be1489b4855a
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-26 13:07:09 +0000

    update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,8 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-
+    return self.__size
 # Example usage
 my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+#print(type(my_square))
+#print(my_square.__dict__)
