4800bd1e0823060e1e999d75ae5d0517d0b1a104
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-26 10:56:09 +0000

    more returns

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:  # Check if the sentence is empty
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
