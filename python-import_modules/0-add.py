573366864e60bea3e1ec26ce9fc4930c7bb5847d
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-26 09:38:49 +0000

    readme

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,13 +3,13 @@
 #function to add a and b
 def add(a, b):
     return a + b
-    
+
 #main function to start from
 def main():
     a = 1
     b = 2
     result = add(a, b)
-    print(f'{a} + {b} = {result}')
+    print('{} + {} = {}'.format(a, b, result))
 
 if __name__ == "__main__":
     main()
