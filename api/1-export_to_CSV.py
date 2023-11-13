e42c1b47d06b7d7577167a131d77d446d6bb41b5
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-11-13 22:12:59 +0000

    export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employees """
+""" Script that uses JSONPlaceholder API to get information about employee """
 import csv
 import requests
 import sys
 
-""""""
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    # Get user ID 
+    # Get user ID from command line arguments
     userid = sys.argv[1]
 
     # Fetch user data from API
@@ -17,26 +16,26 @@ if __name__ == "__main__":
     json_o = res.json()
     username = json_o.get('username')  # Store username
 
-    # Fetch todos  user  API
+    # Fetch todos for user from API
     todos = '{}todos?userId={}'.format(url, userid)
     res = requests.get(todos)
     tasks = res.json()
 
-    # For each todo, keep relevant data in tasking_ing
-    tasking_ing = []
+    # For each todo, store relevant data in l_task
+    l_task = []
     for task in tasks:
-        tasking_ing.append([userid,
+        l_task.append([userid,
                        username,
                        task.get('completed'),
                        task.get('title')])
 
-    # We write data to CSV file
+    # Write data to CSV file
     filename = '{}.csv'.format(userid)
     with open(filename, mode='w') as employee_file:
         employee_writer = csv.writer(employee_file,
                                      delimiter=',',
                                      quotechar='"',
                                      quoting=csv.QUOTE_ALL)
-        for task in tasking_ing:
-            # list in tasking_ing as a new row in the CSV file
+        for task in l_task:
+            # Write each list in l_task as a new row in the CSV file
             employee_writer.writerow(task)
