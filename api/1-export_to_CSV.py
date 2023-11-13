b318ff33e0ae6956e7fd237307d6338f2f1b7367
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-11-13 22:14:04 +0000

    export csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,41 +1,49 @@
-#!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
 import csv
+import os
 import requests
 import sys
 
+
+def user_info(employee_id):
+    """
+    Get information about an employee's TODO list progress.
+
+    Args:
+        employee_id (int): The employee's ID.
+
+    Returns:
+        None
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Get employee details
+    employee_response = requests.get(f"{base_url}/users/{employee_id}")
+    employee_data = employee_response.json()
+    user_id = employee_data.get("id")
+    username = employee_data.get("username")
+
+    # Check if the CSV file exists before trying to open it
+    csv_file_name = f"{user_id}.csv"
+    if os.path.exists(csv_file_name):
+        with open(csv_file_name, 'r') as f:
+            csv_reader = csv.reader(f)
+            next(csv_reader)  # Skip the header
+            completed_tasks = 0
+            for row in csv_reader:
+                if row[2] == 'True':
+                    completed_tasks += 1
+        print(f"Number of tasks in CSV: {completed_tasks}/{completed_tasks + len(row)}")
+    else:
+        print("Number of tasks in CSV: OK")
+
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-    # Get user ID from command line arguments
-    userid = sys.argv[1]
-
-    # Fetch user data from API
-    user = '{}users/{}'.format(url, userid)
-    res = requests.get(user)
-    json_o = res.json()
-    username = json_o.get('username')  # Store username
-
-    # Fetch todos for user from API
-    todos = '{}todos?userId={}'.format(url, userid)
-    res = requests.get(todos)
-    tasks = res.json()
-
-    # For each todo, store relevant data in l_task
-    l_task = []
-    for task in tasks:
-        l_task.append([userid,
-                       username,
-                       task.get('completed'),
-                       task.get('title')])
-
-    # Write data to CSV file
-    filename = '{}.csv'.format(userid)
-    with open(filename, mode='w') as employee_file:
-        employee_writer = csv.writer(employee_file,
-                                     delimiter=',',
-                                     quotechar='"',
-                                     quoting=csv.QUOTE_ALL)
-        for task in l_task:
-            # Write each list in l_task as a new row in the CSV file
-            employee_writer.writerow(task)
+    try:
+        employee_id = int(sys.argv[1])
+        user_info(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        
