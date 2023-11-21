5ac2dc9640d759fbe1d2462bd8f2fd98a008cfd7
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-11-21 00:26:03 +0000

    update

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,10 +12,12 @@ class Square:
      self.__size = size
 
     #to retrieve
+    @property
     def size(self):
         return self.__size
         
     #size.setter
+    @size.setter
     def size(self,value):
         if not isinstance ( value, int):
             raise TypeError("size must be an integer")
