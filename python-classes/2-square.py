d9e40ce21da1d4deb5b14c202db17ab1dd1bd4f7
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-11-21 00:17:46 +0000

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,18 +1,21 @@
+#!/usr/bin/python3
+"""
+Getting the class instance and should be an integer
+"""
+
+
 class Square:
+    """
+Getting the class instance
+"""
+
     def __init__(self, size=0):
-        if not isinstance(size,int):
-           raise TypeError ("size must be an integer")
-
-        if  size < 0:
-            raise ValueError ("size must be >=0") 
-        self.__size = size
-
-  
-
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        return self.__size * self.__size 
-
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
+        return self.__size ** 2
