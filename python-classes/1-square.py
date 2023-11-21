1f130f6f79ca8fe8d8630d98c752a95097cc9bbb
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-11-21 00:15:02 +0000

    update

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,26 +1,18 @@
+#!/usr/bin/python3
+"""
+Getting the class  instance
+"""
+
+
 class Square:
-    """class to handle square of values 
-    pass and return the shit
-    Attributes:
-    size    The value passed to be squared.
     """
+Getting the size
+"""
+
     def __init__(self, size=0):
-        """ initialisation of function to handle prerequire values"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        
-        self.__size = size
-    #print()
-     
-
-my_square_ref = Square
-#print(my_square_ref(3))
-# type(my_square_ref)
-def main():
-    my_square_ref = Square
-    
-
-main()
-__init__
+        else:
+            self.__size = size
