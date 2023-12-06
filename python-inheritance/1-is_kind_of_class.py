438b1b705c93d30e83d4608adbca6e3a27e9efde
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-12-06 10:31:59 +0000

    update

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,30 +1,62 @@
-# finding out if same class or inherit 
-# class has an object we know of
+"""Module for checking class relationships."""
+
+
 def is_kind_of_class(obj, a_class):
+    """
+    Check if the object is an instance of the specified class or a subclass of it.
+
+    Parameters:
+    - obj: The object to check.
+    - a_class: The class to compare the object's type with.
+
+    Returns:
+    - True if the object is an instance of the specified class or a subclass of it, False otherwise.
+    """
     return isinstance(obj, a_class) or issubclass(type(obj), a_class)
 
-# parent class
+# Parent class
+
+
 class A:
+    """Parent class A."""
     pass
 
-#inherit class
+# Inherited class
+
+
 class B(A):
+    """Inherited class B."""
     pass
 
-#a class
-class C():
+# Another class
+
+
+class C:
+    """Class C."""
     pass
 
-#objects assigned
+
+# Objects assigned
 obj1 = A()
 obj2 = B()
 obj3 = C()
 
+# Test the function
+result1 = is_kind_of_class(obj1, A)  # True, obj1 is an instance of A
+result2 = is_kind_of_class(obj2, A)  # True, obj2 is a subclass of A
+result3 = is_kind_of_class(obj2, B)  # True, obj2 is an instance of B
+result4 = is_kind_of_class(obj3, C)  # True, obj3 is an instance of C
+# False, obj3 is not an instance or subclass of A
+result5 = is_kind_of_class(obj3, A)
+
+# Test with a non-class object
 a = 1
-#output
-# print(is_kind_of_class(obj1, A)) 
-# print(is_kind_of_class(obj2, B)) 
-# print(is_kind_of_class(obj2, A)) 
-# print(is_kind_of_class(obj1, B)) 
-# print(is_kind_of_class(obj3, A)) 
-# print(is_kind_of_class(obj3, C))  
+result6 = is_kind_of_class(a, int)  # True, 'a' is an instance of int
+
+# Print results
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
+# print(result5)
+# print(result6)
