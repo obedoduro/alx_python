e82894ab71af8d206eaa24ff770c312a8f452ce3
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-12-06 10:48:53 +0000

    update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,25 @@
-"""Module to check Geometry"""
+"""Module for Geometry Operations."""
+
+
 class BaseGeometry:
-    """Inherited class BaseGeometry"""
-    pass
+    """
+    Base class for geometry-related operations.
 
+    This class is intended to be inherited by other classes representing specific geometric shapes.
+    """
+
+    def area(self):
+        """
+        Calculate the area.
+
+        Raises:
+        - NotImplementedError: This method should be implemented by subclasses.
+        """
+        raise NotImplementedError(
+            "Subclasses must implement the 'area' method.")
+
+
+# Example usage
 bg = BaseGeometry()
-
-# print(bg)
+# Uncommenting the line below would raise a NotImplementedError since 'area' is not implemented in the BaseGeometry class.
+# print(bg.area())
