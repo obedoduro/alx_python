7ae6d27fa74915b97dc6c89021414235212063d2
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-12-06 10:35:50 +0000

    update

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,32 +1,62 @@
-#only sub class of a class
+"""Module for checking class relationships."""
+
+
 def inherits_from(obj, a_class):
+    """
+    Check if the object is a subclass of the specified class.
+
+    Parameters:
+    - obj: The object to check.
+    - a_class: The class to compare the object's type with.
+
+    Returns:
+    - True if the object is a subclass of the specified class, False otherwise.
+    """
     return isinstance(obj, a_class) and type(obj) != a_class
 
-#parent class
+# Parent class
+
+
 class A:
+    """Parent class A."""
     pass
-#inherited directly
+
+# Inherited directly
+
+
 class B(A):
+    """Inherited class B."""
     pass
-#inherited indirectly
+
+# Inherited indirectly
+
+
 class C(B):
+    """Inherited class C, indirectly from A."""
     pass
 
-#a class
-class D():
+# Another class
+
+
+class D:
+    """Class D."""
     pass
 
-#assign objects
+
+# Assign objects
 obj1 = A()
 obj2 = B()
 obj3 = C()
 obj4 = D()
 
+# Test the function
+result1 = inherits_from(obj1, A)  # False, obj1 is not a subclass of A
+result2 = inherits_from(obj2, A)  # True, obj2 is a subclass of A
+result3 = inherits_from(obj3, A)  # True, obj3 is a subclass of A through B
+result4 = inherits_from(obj4, A)  # False, obj4 is not a subclass of A
 
-
-
-print(" {}", None)
-# print(inherits_from(obj2, B))
-# print(inherits_from(obj3, A)) 
-# print(inherits_from(obj3, B)) 
-# print(inherits_from(obj4, D)) 
+# Print results
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
