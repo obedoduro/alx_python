ff864300a9ecb2a3d2550609d14386dc742d0fe7
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-12-06 10:24:27 +0000

    update

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,18 +1,40 @@
-#checking instance of object in each class
-def is_same_class(obj, a_class):
-#    print(".")
-   return type(obj) is a_class
+"""Module with a function for checking the instance of an object in a class."""
 
-# Classes 
+
+def is_same_class(obj, a_class):
+    """
+    Check if the type of the given object is the same as the specified class.
+
+    Parameters:
+    - obj: The object to check.
+    - a_class: The class to compare the object's type with.
+
+    Returns:
+    - True if the object's type is the same as the specified class, False otherwise.
+    """
+    return type(obj) is a_class
+
+# Classes
+
+
 class A:
-    pass
+    """Class A."""
+
 
 class B(A):
-    pass
+    """Class B, which is a subclass of A."""
+
 
 obj1 = A()
 obj2 = B()
 
-is_same_class(obj1, A)
+result1 = is_same_class(obj1, A)  # True
+result2 = is_same_class(obj2, A)  # False
+result3 = is_same_class(obj2, B)  # True
+
+print(result1)
+print(result2)
+print(result3)
+
 a = 1
-#print (  is_same_class(a, int))
+print(is_same_class(a, int))  # True
