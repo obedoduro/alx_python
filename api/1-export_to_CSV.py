08c7cb604fd2e3d9fc582be8ce2eaa8d3e6131fa
Obed Oduro <oduroadjeiobed16@gmail.com>
2024-02-22 22:59:13 +0000

    api push

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,54 +1,51 @@
-#!/usr/bin/python3
+"""fetching employee,TODO lists and counting completed tasks
 """
-    Python script that exports data in the CSV format
-"""
+
 import csv
-import json
 import requests
-from sys import argv
+import sys
+
+def fetch_user_info(user_id):
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_url = '{}users/{}'.format(url, user_id)
+    response = requests.get(user_url)
+    user_info = response.json()
+    return user_info
+
+
+def fetch_user_todos(user_id):
+    url = 'https://jsonplaceholder.typicode.com/'
+    todos_url = '{}todos?userId={}'.format(url, user_id)
+    response = requests.get(todos_url)
+    todos = response.json()
+    return todos
+
+
+def export_to_csv(user_id, user_info, todos):
+    filename = '{}.csv'.format(user_id)
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME',
+                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for todo in todos:
+            writer.writerow({
+                'USER_ID': user_id,
+                'USERNAME': user_info['username'],
+                'TASK_COMPLETED_STATUS': 'Completed' if todo['completed'] else 'Incomplete',
+                'TASK_TITLE': todo['title']
+            })
+    print(f"CSV file '{filename}' has been created successfully.")
 
 
 if __name__ == "__main__":
-    """
-        Request user info by employee ID
-    """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
-    """
-        Convert json to dictionary
-    """
-    user = json.loads(request_employee.text)
-    """
-        Extract username
-    """
-    username = user.get("username")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <user_id>")
+        sys.exit(1)
 
-    """
-        Request user's TODO list
-    """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
-    """
-        Dictionary to store task status(completed) in boolean format
-    """
-    tasks = {}
-    """
-        Convert json to list of dictionaries
-    """
-    user_todos = json.loads(request_todos.text)
-    """
-        Loop through dictionary & get completed tasks
-    """
-    for dictionary in user_todos:
-        tasks.update({dictionary.get("title"): dictionary.get("completed")})
-
-    """
-        Export to CSV
-    """
-    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
-        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for k, v in tasks.items():
-            file_editor.writerow([argv[1], username, v, k])
-
-    print('Data exported to {}.csv'.format(argv[1]))
-
+    user_id = sys.argv[1]
+    user_info = fetch_user_info(user_id)
+    todos = fetch_user_todos(user_id)
+    export_to_csv(user_id, user_info, todos)
