7aa72bfef15c37f535da86e5925463e82a5ac2af
Obed Oduro <oduroadjeiobed16@gmail.com>
2024-02-22 23:04:43 +0000

    api push

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,52 +1,42 @@
-#!/usr/bin/python3
-"""
-    python script that exports data in the JSON format
-"""
 import json
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    """
-        request user info by employee ID
-    """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-    """
-        convert json to dictionary
-    """
-    user = json.loads(request_employee.text)
-    """
-        extract username
-    """
-    username = user.get("username")
+    if len(argv) != 2:
+        print("Usage: python script.py USER_ID")
+        exit(1)
 
-    """
-        request user's TODO list
-    """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
-    """
-        list to store task status(completed) in dictionary format
-    """
-    tasks = []
-    """
-        convert json to list of dictionaries
-    """
-    user_todos = json.loads(request_todos.text)
-    """
-        loop through dictionary & get completed tasks
-    """
-    for dictionary in user_todos:
-        task = {}
-        task["task"] = dictionary.get("title")
-        task["completed"] = dictionary.get("completed")
-        task["username"] = username
-        tasks.append(task)
+    user_id = argv[1]
 
-    """
-        export to JSON
-    """
-    with open('{}.json'.format(argv[1]), 'w') as json_file:
-        json.dump({argv[1]: tasks}, json_file)
+    try:
+        request_employee = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/'.format(user_id))
+        request_todos = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id))
+
+        if request_employee.status_code != 200 or request_todos.status_code != 200:
+            print("Failed to retrieve data. Please verify the USER_ID.")
+            exit(1)
+
+        user = json.loads(request_employee.text)
+        username = user.get("username")
+        user_todos = json.loads(request_todos.text)
+
+        tasks = []
+
+        for todo in user_todos:
+            task = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": username
+            }
+            tasks.append(task)
+
+        with open('{}.json'.format(user_id), 'w') as json_file:
+            json.dump({user_id: tasks}, json_file)
+
+        print("Correct USER_ID: OK")
+
+    except Exception as e:
+        print("An error occurred:", e)
