bade7e3c9d7eb206f3259d3acdca4a153c1a24ed
Obed Oduro <oduroadjeiobed16@gmail.com>
2024-02-22 23:43:23 +0000

    api push

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,41 +7,51 @@ import requests
 from sys import argv
 
 
-def export_tasks_to_json(user_id):
-    """
-    Export tasks owned by a specific user to a JSON file.
+def export_tasks_to_json(user_id, tasks):
+    data = {user_id: []}
+    for task in tasks:
+        task_info = {
+            "task": task.get("title"),
+            "completed": task.get("completed"),
+            "username": user_info.get("username")
+        }
+        data[user_id].append(task_info)
 
-    Args:
-        user_id (int): The ID of the user whose tasks need to be exported.
-
-    Returns:
-        None
-    """
-    url = 'https://jsonplaceholder.typicode.com/todos'
-    params = {'userId': user_id}
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        tasks = response.json()
-        user_tasks = []
-
-        for task in tasks:
-            user_tasks.append({
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": task['username']
-            })
-
-        filename = f"{user_id}.json"
-        with open(filename, 'w') as json_file:
-            json.dump({user_id: user_tasks}, json_file, indent=4)
-
-        print(f"Tasks exported to {filename} successfully.")
-    else:
-        print(
-            f"Failed to retrieve tasks for user {user_id}. Status code: {response.status_code}")
+    with open(f"{user_id}.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
 
 
 if __name__ == "__main__":
-    user_id = input("Enter user ID: ")
-    export_tasks_to_json(user_id)
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    user_id = sys.argv[1] if len(sys.argv) > 1 else None
+
+    if user_id is None:
+        print("Please provide a user ID as a command-line argument.")
+        sys.exit(1)
+
+    user_endpoint = f"{url}users/{user_id}"
+    todos_endpoint = f"{url}todos?userId={user_id}"
+
+    user_res = requests.get(user_endpoint)
+    if user_res.status_code != 200:
+        print(f"Failed to retrieve user data for user ID {user_id}.")
+        sys.exit(1)
+
+    user_info = user_res.json()
+
+    print(f"Employee {user_info.get('name')} is done with tasks", end="")
+
+    tasks_res = requests.get(todos_endpoint)
+    if tasks_res.status_code != 200:
+        print(f"Failed to retrieve tasks data for user ID {user_id}.")
+        sys.exit(1)
+
+    tasks = tasks_res.json()
+    completed_tasks = [task for task in tasks if task.get("completed")]
+
+    print(f" ({len(completed_tasks)}/{len(tasks)}):")
+    for task in completed_tasks:
+        print(f"\t{task.get('title')}")
+
+    export_tasks_to_json(user_id, tasks)
